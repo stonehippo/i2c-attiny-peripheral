f080d3fdbcffc75a8527fef413e55f1af6dcad60
George White <stonehippo@gmail.com>
2021-01-20 13:51:06 -0500

    First pass at working i2c peripheral with ATTinyCore

---
+++ i2c-attiny-peripheral.ino
@@ -0,0 +1,20 @@
+#include <Wire.h>
+
+word readValue = 0;
+
+void setup() {
+	Wire.begin(0x15);
+	Wire.onRequest(wireRequest);
+	pinMode(1, OUTPUT);
+}
+
+void loop() {
+	readValue = analogRead(A2);
+	analogWrite(1, map(readValue,0,1023,0,255));
+}
+
+void wireRequest(int bytes) {
+	byte data[] = {highByte(readValue), lowByte(readValue)};
+	Wire.write(data,2);
+}
+
